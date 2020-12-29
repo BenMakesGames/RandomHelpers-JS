d95320ee6004c0852effeab456468564a7c25c54
Ben H <ben@benmakesgames.com>
2020-12-29 14:16:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+Math.randomInt = function(exclusiveMax) {
+    return Math.floor(Math.random() * exclusiveMax);
+};
+
+Math.randomDiceRoll = function(numberOfDice, diceSize) {
+    let total = 0;
+
+    for(let i = 0; i < numberOfDice; i++)
+        total += Math.randomInt(diceSize);
+
+    return total;
+};
+
+Math.randomFromList = function(list) {
+    return list[Math.randomInt(list.length)];
+};
+
+Math.randomString = function(allowedCharacters, length) {
+    let string = '';
+
+    for(let i = 0; i < length; i++)
+        string += allowedCharacters[Math.randomInt(allowedCharacters.length)];
+};
+
+Math.randomBool = function() {
+    return Math.random() < 0.5;
+};
